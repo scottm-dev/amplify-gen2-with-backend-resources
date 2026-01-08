6fc9682dfb93412e43cf5b9504850aa069163d16
Scott McDaniel <mcscottm@amazon.com>
2026-01-07 20:48:41 -0800

    add ai

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -9,6 +9,28 @@ const schema = a.schema({
       isDone: a.boolean(),
     })
     .authorization((allow) => [allow.publicApiKey()]),
+
+  chat: a.conversation({
+    aiModel: a.ai.model('Claude 3.5 Haiku'),
+    systemPrompt: 'You are a helpful assistant',
+  })
+    .authorization((allow) => allow.owner()),
+
+  generateRecipe: a.generation({
+    aiModel: a.ai.model('Claude 3.5 Haiku'),
+    systemPrompt: 'You are a helpful assistant that generates recipes.',
+  })
+  .arguments({
+    description: a.string(),
+  })
+  .returns(
+    a.customType({
+      name: a.string(),
+      ingredients: a.string().array(),
+      instructions: a.string(),
+    })
+  )
+  .authorization((allow) => allow.authenticated()),
 });
 
 // Used for code completion / highlighting when making requests from frontend
